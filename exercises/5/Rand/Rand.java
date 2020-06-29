4c29befd2c74860856cee16f6a3662c493c47867
wonsunahn <wahn@pitt.edu>
2020-06-29 17:00:03 -0400

    Update Rand.java

--- exercises/5/Rand/Rand.java
+++ exercises/5/Rand/Rand.java
@@ -24,15 +24,15 @@ public class Rand {
     Random random = new Random();      // (1)
 
     int a = random.nextInt(2);           // (2)
-    System.out.printf("a=%d\n", a);
+    System.out.printf("a=%d%n", a);
 
     //... lots of code here
 
     int b = random.nextInt(3);           // (3)
-    System.out.printf("  b=%d       ,a=%d\n", b, a);
+    System.out.printf("  b=%d       ,a=%d%n", b, a);
 
     int c = a/(b+a -2);                  // (4)
 
-    System.out.printf("=>  c=%d     , b=%d, a=%d\n", c, b, a);         
+    System.out.printf("=>  c=%d     , b=%d, a=%d%n", c, b, a);         
   }
 }
